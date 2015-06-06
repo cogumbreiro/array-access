a05dcc9a4b68f0d1338b66fba537bba7afd4c622
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-06-05 20:55:42 -0500

    Use the rust backend.

--- gen-dataset-3/main.sh
+++ gen-dataset-3/main.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-find dataset-2  -name "*.c.txt" -exec sh -c './gen-dataset-3.py "$1" > "${1%.c.txt}.jsonl"' _ {} \;
+for f in dataset-2/*.c.txt; do
+    ./gen-dataset-3.py "$f" | target/release/array_access > "${f%.c.txt}.jsonl"
+done
 mv dataset-2/*.jsonl dataset-3
